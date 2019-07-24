8c4102279246f15f43ae7877e0105629ae12e4bf
Zach Masson <zachmasson96@gmail.com>
2019-07-24 22:45:05 +0000

    final

--- Isotopes (LL, DA, ST).R
+++ Isotopes (LL, DA, ST).R
@@ -1,20 +1,14 @@
-attach(fensterer_CM)
-SantoTomasCM <- fensterer_CM
-mydata <- mutate(mydata, group = factor(group), community = factor(community))
 library(ggplot2)
 
 ## Santo Tomas (fensterer_CM)
+attach(fensterer_CM)
+SantoTomasCM <- fensterer_CM
 ggplot(SantoTomasCM,
        aes(x = SantoTomasCM$`age[ka]`, y = d18O))+
   geom_line()
 ggplot(SantoTomasCM,
        aes(x = SantoTomasCM$`age[ka]`, y = d13C))+
   geom_line()
-
-
-##need to fix these maybe...
-plot(`age[ka]`,d13C,type="l",col="red")
-plot(`age[ka]`,d18O,type="l",col="blue")
 
 ## Dos Anas (fensterer_Cp)
 attach(Fensterer_CP)
@@ -26,7 +20,6 @@ ggplot(DosAnasCP,
        aes(x = DosAnasCP$`cal yr BP (age before 1950)`, y = d13C))+
   geom_line()
 
-
 ## Laguna de la Leche
 ## attach the NEW LL isotopes file from google drive! 
 attach(LL_ISOTOPES_final)
@@ -37,22 +30,24 @@ ggplot(LL,
 ggplot(LL,
        aes(x = LL_ISOTOPES_final$Ages, y = `Carbon13`))+
   geom_line()
-plot(LL_ISOTOPES_final$Ages,`Carbon13`,type="l",col="red")
-plot(LL_ISOTOPES_final$Ages,`Oxygen18`,type="l",col="blue")
-
 
 ##correcting the ages of Santo Tomas CM
 SantoTomasCM$`age[ka]` <- SantoTomasCM$`age[ka]` * 1000
 
-
 ## layering the plots (oxygen18)
-ggplot(SantoTomasCM, aes(x = SantoTomasCM$`age[ka]`, y = SantoTomasCM$d18O))+ geom_line(aes(color = 'blue'))+
-  geom_line(data = DosAnasCP, aes(x = DosAnasCP$`cal yr BP (age before 1950)`, y = DosAnasCP$d18O, color = 'red'))+
-  geom_line(data = LL_ISOTOPES_final, aes(x = LL_ISOTOPES_final$Ages, y = LL_ISOTOPES_final$Oxygen18, color = 'green'))
+ggplot(SantoTomasCM, aes(x = SantoTomasCM$`age[ka]`, y = SantoTomasCM$d18O))+ geom_line(aes(color = 'Santo Tomas'))+
+  geom_line(data = DosAnasCP, aes(x = DosAnasCP$`cal yr BP (age before 1950)`, y = DosAnasCP$d18O, color = 'Dos Anas'))+
+  geom_line(data = LL_ISOTOPES_final, aes(x = LL_ISOTOPES_final$Ages, y = LL_ISOTOPES_final$Oxygen18, color = 'Laguna de la Leche'))+
+  xlab("Age")+
+  ylab("Oxygen 18")+
+  ggtitle("O18 Record from 3 Sites in Cuba")
 
 ## layering the plots (carbon13)
-ggplot(SantoTomasCM, aes(x = SantoTomasCM$`age[ka]`, y = SantoTomasCM$d13C))+ geom_line(aes(color = 'blue'))+
-  geom_line(data = DosAnasCP, aes(x = DosAnasCP$`cal yr BP (age before 1950)`, y = DosAnasCP$d13C, color = 'red'))+
-  geom_line(data = LL_ISOTOPES_final, aes(x = LL_ISOTOPES_final$Ages, y = LL_ISOTOPES_final$Carbon13, color = 'green'))
+ggplot(SantoTomasCM, aes(x = SantoTomasCM$`age[ka]`, y = SantoTomasCM$d13C))+ geom_line(aes(color = 'Santo Tomas'))+
+  geom_line(data = DosAnasCP, aes(x = DosAnasCP$`cal yr BP (age before 1950)`, y = DosAnasCP$d13C, color = 'Dos Anas'))+
+  geom_line(data = LL_ISOTOPES_final, aes(x = LL_ISOTOPES_final$Ages, y = LL_ISOTOPES_final$Carbon13, color = 'Laguna de la Leche'))+
+  xlab("Age")+
+  ylab("Carbon 13")+
+  ggtitle("C13 Record from 3 Sites in Cuba")
 
 
